9e01a2ba330e2a13435ea72d0d6e41e294e232ec
sergey-miryanov <sergey.miryanov@gmail.com>
2010-05-30 23:49:39 +0600

    Initial commit

---
+++ resources/public/javascript/tryclojure.js
@@ -0,0 +1,56 @@
+function setupLink(url) {
+    return function(e) { $("#changer").load(url, function(data) { $("#changer").html(data); }); }
+}
+
+function setupExamples(controller) {
+    $(".code").click(function(e) {
+	controller.promptText($(this).text());
+    });
+}
+
+function getStep(n, controller) {
+    $("#tuttext").load("tutorial", { step: n }, function() { setupExamples(controller); });
+}
+
+$(document).ready(
+    function() {
+	var controller = $("#console").console({
+	    promptLabel: 'Clojure> ',
+	    commandValidate:function(line){
+		if (line == "") return false;
+		else return true;
+	    },
+	    commandHandle:function(line){
+		var data;
+		jQuery.ajax({
+		    url: "http://www.try-clojure.org/magics" + "?code=" + encodeURIComponent(line),
+		    success: function(result) {data = result;},
+		    async:   false
+		}); 
+		return [{msg: data,
+			 className:"jquery-console-message-value"}];
+	    },
+	    welcomeMessage:'Enter some Clojure code, and it will be evaluated.',
+	    autofocus:true,
+	    animateScroll:true,
+	    promptHistory:true
+	});
+
+	$("#about").click(setupLink("about"));
+	$("#links").click(setupLink("links"));
+	$("#tutorial").click(function(e) {
+	    $("#changer").load("tutorial", {step: 0}, function(data) { 
+		var step = 1;
+		$("#continue").click(function(e) {
+		    if(step < 6 ) { step += 1; }
+		    getStep(step, controller);
+		    $("#tuttext").scrollTop(0);
+		});
+		$("#back").click(function(e) {
+		    if(step > 1) { step -= 1; }
+		    getStep(step, controller);
+		    $("#tuttext").scrollTop(0);
+		});
+	    });
+	});
+    });
